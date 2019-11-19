425ae3a97fe2b6ff6db86184b20bad8e7877fde6
harish <harish.vanama@wavemaker.com>
2019-11-19 18:34:18 +0530

    aaa

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -7,8 +8,18 @@ import org.testng.annotations.Test;
  */
 public class SampleTest {
 
-    @Test(invocationCount = 100)
-    public void sample(){
+    @DataProvider(parallel = true)
+    public static Object[][] browser() {
+
+        Object[][] objects = new Object[100][1];
+        for (int i = 0; i < 20; i++) {
+            objects[i][0] = "test";
+        }
+        return objects;
+    }
+
+    @Test(dataProvider = "browser")
+    public void sample(String a) {
         System.out.println("Browser opened");
         WebDriver firefox = DriverFactory.createDriver("Firefox");
         firefox.get("https://www.google.com");
